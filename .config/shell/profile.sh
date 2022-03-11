c7c117a7664661cf7f343489cd5419c6596a4585
ia rigby <marvinzem@gmail.com>
2022-03-11 18:12:40 +0400

    npm instill gi

--- .config/shell/profile.sh
+++ .config/shell/profile.sh
@@ -20,3 +20,6 @@ export HISTFILESIZE=1000000
 export HISTCONTROL=ignoredups
 setopt APPEND_HISTORY
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
