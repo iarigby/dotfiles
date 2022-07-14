e6c1a0fef63b2f7a89085fe56000695352ad37e7
ia rigby <marvinzem@gmail.com>
2022-07-14 16:58:15 +0200

    fix nvm for mac, add environment file

--- .config/shell/profile.sh
+++ .config/shell/profile.sh
@@ -7,6 +7,7 @@ fi
 source $SHELL_CONFIG/aliases.sh
 source $SHELL_CONFIG/path.sh
 source $SHELL_CONFIG/functions.sh
+source $SHELL_CONFIG/environment.sh
 
 export MANPATH="/usr/local/man:$MANPATH"
 
@@ -25,3 +26,4 @@ setopt APPEND_HISTORY
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source $(brew --prefix nvm)/nvm.sh
